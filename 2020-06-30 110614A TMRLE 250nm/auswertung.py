e58afe434bd85919cb6749031f8aed9c015b6525
Raphael Kaiser <raphaelricokaiser@web.de>
2020-08-25 13:57:28 +0200

    plot

--- 2020-06-30 110614A TMRLE 250nm/auswertung.py
+++ 2020-06-30 110614A TMRLE 250nm/auswertung.py
@@ -85,14 +85,13 @@ mittelwert_matrix_new_neg = mittelwert_matrix_neg[:,15:244]
 #print(mittelwert_matrix_new.shape)  # (1024,229)
 #**************************
 
-#Wavelength
+# Set wavelength
 wl=np.genfromtxt('775nm_grating3_wellenlängenbereich.txt',usecols=(0)) #only takes the first column (spalte)
-#Angle
+# Set angle
 theta=np.linspace(-23.578,23.578,256)
 theta_new=np.linspace(-23.578,23.578,mittelwert_matrix_new.shape[1]) # because of the greatness of the sensor field
 
 #***************************************************************************************************************************
-
 #Colormap plot
 plt.pcolormesh(theta_new,wl,mittelwert_matrix_new,cmap='hot') # make the axes correct x,y,matrix
 plt.gca().invert_yaxis()                            #inverts the y axis
@@ -105,7 +104,6 @@ cbar.set_label('Intensity')
 #save figure and show figure
 #plt.show()
 plt.savefig('build/photolumineszenz.png')
-
 
 #***************************************************************************************************************************
 #Intensitätsunterschied rho berechnen 
@@ -125,6 +123,7 @@ cbar.set_label('relative Intensity')
 #save figure and show figure
 #plt.show()
 plt.savefig('build/rel_aenderung.png')
+
 #***************************************************************************************************************************
 # program to select a certain wavelength and to plot the relative intensity againt the angle
 
@@ -133,22 +132,28 @@ wavelenght = 740
 minimized_array = abs(wl-wavelenght)            # minimal array
 #print(minimized_array)
 
-minimum_value=np.amin(minimized_array)          # minimum_value of array
+minimum_value = np.amin(minimized_array)          # find minimum_value of array
 #print(minimum_value)
 
-index=np.where(minimized_array == minimum_value)# index of the minimum
+index = np.where(minimized_array == minimum_value)# index of the minimum
 #print(index[0][0])                              # only index 
-index=index[0][0]
+index = index[0][0]
 print('You selected the wavelength:', wl[index],'nm.', 'The wavelength you wanted was: ', wavelenght,'nm.')
 
 
 #careful fails at randwerte --> out of bounds
 shift = 20
 lower = index - shift
-upper = index + shift + 1       #because of upper bound in mean_area_rho
+upper = index + shift + 1       #because of upper bound in mean_area_rho, see next line!
 
-mean_area_rho = np.mean(rho[lower:upper,:],axis=0)
+mean_area_rho = np.mean(rho[lower:upper,:],axis=0) #axis=0 is command for mean. otherwise it would give me only one value instead of a list.
+#check
 print(np.shape(rho[lower:upper,:]))
+print(mean_area_rho)
+print(rho[lower:upper,:])
+print(np.shape(rho[lower:upper,:]))
+
+
 
 #plot
 plt.clf()
@@ -164,5 +169,36 @@ plt.ylabel(r'$\rho$')
 plt.title('Messung bei einer Wellelänge von %i nm' % wavelenght)
 
 #save
-plt.savefig('build/specific_wavelength_%i_nm.png' % wavelenght)
+plt.savefig('build/rho_at_specific_wavelength_%i_nm.png' % wavelenght)
+#***************************************************************************************************************************
+
+#***************************************************************************************************************************
+# progam to plot I against theta at a certain wavelength for positive an negative B field
+
+#plot
+plt.clf()
+plt.grid()
+plt.minorticks_on()
+
+# Mittelwert der Intersität über den Bereich von vgl. lower upper.
+#axis=0 is command for mean. otherwise it would give me only one value instead of a list.
+
+#pos intensity
+mean_intensity_pos = np.mean(mittelwert_matrix_new_pos[lower:upper,:],axis=0)
+plt.plot(theta_new,mean_intensity_pos,'r-',label='pos. Magnetfeld')
+
+#neg Intensity
+mean_intensity_neg = np.mean(mittelwert_matrix_new_neg[lower:upper,:],axis=0)
+plt.plot(theta_new,mean_intensity_neg,'b-',label='neg. Magnetfeld')
+
+#format
+plt.xlim(theta_new[0], theta_new[-1])
+plt.xlabel(r'$\theta / \mathrm{°}$')
+plt.ylabel(r'$I$')
+plt.legend(loc='best')
+
+plt.title('Intensität bei einer Wellenlämge von %i nm.' % wavelenght)
+
+#save
+plt.savefig('build/positive_and_negative_intensity_at_specific_wavelength_%i_nm.png' % wavelenght)
 #***************************************************************************************************************************
